ade091b314f6f3513526e0c0f278c531a978ce3f
os-tangkai <os-tangkai@jsfund.cn>
2018-02-02 11:40:23 +0800

    使用pug模板渲染首页

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import * as koa from 'koa';
-import * as pug from 'pug';
-import tpl from './tpl';
-
-const { ejs: ejsTpl, pug: pugTpl } = tpl;
+import * as views from 'koa-views';
+import { resolve } from 'path';
 
 const app = new koa();
 
+app.use(views(resolve(__dirname, '../views'), { extension: 'pug' }));
+
 app.use(async (ctx: koa.Context, next) => {
-  ctx.body = pug.render(pugTpl, {
+  await ctx.render('index', {
     you: '张三',
-    me: '李四',
+    me: '李四111',
   });
 });
 
--- src/tpl/index.ts
+++
@@ -1,9 +0,0 @@
-import normal from './normal';
-import ejs from './ejs';
-import pug from './pug';
-
-export default {
-  normal,
-  ejs,
-  pug,
-};
